a3c85832bada6280c284579f831b0617a9ec1856
Amira Zayn <amirazayn5@gmail.com>
2023-12-29 23:21:41 +0200

    update in authentication

--- src/main/java/com/example/order_management/controllers/AuthenticationController.java
+++ src/main/java/com/example/order_management/controllers/AuthenticationController.java
@@ -5,7 +5,7 @@ import com.example.order_management.services.AuthenticationService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/authentication")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
@@ -32,7 +32,7 @@ public class AuthenticationController {
         return "Logout successful";
     }
 
-    @RequestMapping("/status")
+    @GetMapping("/status")
     public String status() {
         return authenticationService.isLoggedIn() ? "User is logged in" : "User is not logged in";
     }
